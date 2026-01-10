d3873428770fe0ca961b8246771eb51919156448
25006675-png <25006675@siswa.um.edu.my>
2026-01-10 17:19:32 +0800

    set to english

--- src/App.java
+++ src/App.java
@@ -1,10 +1,10 @@
+import java.util.Locale;
+
 import javafx.application.Application;
 
 public class App {
     public static void main(String[] args) {
-        // Force English Locale for consistent UI (DatePickers, Alerts, WeekFields)
-        java.util.Locale.setDefault(java.util.Locale.ENGLISH);
+        Locale.setDefault(Locale.ENGLISH);
         Application.launch(CalendarGUI.class, args);
     }
 }
-
